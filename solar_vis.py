57563edee39975e2f64c785c863098f29010c4a3
ArtMurMax <Muravev.AM@phystech.edu>
2019-11-13 08:14:41 +0300

    All spaces were replaced by tabs & some unimportant changes in solar_objects.py

--- solar_vis.py
+++ solar_vis.py
@@ -22,98 +22,98 @@ scale_factor = None
 
 
 def calculate_scale_factor(max_distance):
-    """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
-    global scale_factor
-    scale_factor = 0.4*min(window_height, window_width)/max_distance
-    print('Scale factor:', scale_factor)
+	"""Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
+	global scale_factor
+	scale_factor = 0.4*min(window_height, window_width)/max_distance
+	print('Scale factor:', scale_factor)
 
 
 def scale_x(x):
-    """Возвращает экранную **x** координату по **x** координате модели.
-    Принимает вещественное число, возвращает целое число.
-    В случае выхода **x** координаты за пределы экрана возвращает
-    координату, лежащую за пределами холста.
+	"""Возвращает экранную **x** координату по **x** координате модели.
+	Принимает вещественное число, возвращает целое число.
+	В случае выхода **x** координаты за пределы экрана возвращает
+	координату, лежащую за пределами холста.
 
-    Параметры:
+	Параметры:
 
-    **x** — x-координата модели.
-    """
+	**x** — x-координата модели.
+	"""
 
-    return int(x * scale_factor) + window_width // 2
+	return int(x * scale_factor) + window_width // 2
 
 
 def scale_y(y):
-    """Возвращает экранную **y** координату по **y** координате модели.
-    Принимает вещественное число, возвращает целое число.
-    В случае выхода **y** координаты за пределы экрана возвращает
-    координату, лежащую за пределами холста.
-    Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
+	"""Возвращает экранную **y** координату по **y** координате модели.
+	Принимает вещественное число, возвращает целое число.
+	В случае выхода **y** координаты за пределы экрана возвращает
+	координату, лежащую за пределами холста.
+	Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
 
-    Параметры:
+	Параметры:
 
-    **y** — y-координата модели.
-    """
+	**y** — y-координата модели.
+	"""
 
-    return int(y * scale_factor) + window_width // 2
+	return int(y * scale_factor) + window_width // 2
 
 
 def create_star_image(space, star):
-    """Создаёт отображаемый объект звезды.
+	"""Создаёт отображаемый объект звезды.
 
-    Параметры:
+	Параметры:
 
-    **space** — холст для рисования.
-    **star** — объект звезды.
-    """
+	**space** — холст для рисования.
+	**star** — объект звезды.
+	"""
 
-    x = scale_x(star.x)
-    y = scale_y(star.y)
-    r = star.R
-    star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
+	x = scale_x(star.x)
+	y = scale_y(star.y)
+	r = star.R
+	star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
 
 
 def create_planet_image(space, planet):
-    """Создаёт отображаемый объект планеты.
+	"""Создаёт отображаемый объект планеты.
 
-    Параметры:
+	Параметры:
 
-    **space** — холст для рисования.
-    **planet** — объект планеты.
-    """
-    x = scale_x(planet.x)
-    y = scale_y(planet.y)
-    r = planet.R
-    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
+	**space** — холст для рисования.
+	**planet** — объект планеты.
+	"""
+	x = scale_x(planet.x)
+	y = scale_y(planet.y)
+	r = planet.R
+	planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
 
 def update_system_name(space, system_name):
-    """Создаёт на холсте текст с названием системы небесных тел.
-    Если текст уже был, обновляет его содержание.
+	"""Создаёт на холсте текст с названием системы небесных тел.
+	Если текст уже был, обновляет его содержание.
 
-    Параметры:
+	Параметры:
 
-    **space** — холст для рисования.
-    **system_name** — название системы тел.
-    """
-    space.create_text(30, 80, tag="header", text=system_name, font=header_font)
+	**space** — холст для рисования.
+	**system_name** — название системы тел.
+	"""
+	space.create_text(30, 80, tag="header", text=system_name, font=header_font)
 
 
 def update_object_position(space, body):
-    """Перемещает отображаемый объект на холсте.
+	"""Перемещает отображаемый объект на холсте.
 
-    Параметры:
+	Параметры:
 
-    **space** — холст для рисования.
-    **body** — тело, которое нужно переместить.
-    """
-    x = scale_x(body.x)
-    y = scale_y(body.y)
-    r = body.R
-    if x + r < 0 or x - r > window_width or y + r < 0 or y - r > window_height:
-        space.coords(body.image, window_width + r, window_height + r,
-                     window_width + 2*r, window_height + 2*r)  # положить за пределы окна
-    space.coords(body.image, x - r, y - r, x + r, y + r)
+	**space** — холст для рисования.
+	**body** — тело, которое нужно переместить.
+	"""
+	x = scale_x(body.x)
+	y = scale_y(body.y)
+	r = body.R
+	if x + r < 0 or x - r > window_width or y + r < 0 or y - r > window_height:
+		space.coords(body.image, window_width + r, window_height + r,
+					 window_width + 2*r, window_height + 2*r)  # положить за пределы окна
+	space.coords(body.image, x - r, y - r, x + r, y + r)
 
 
 if __name__ == "__main__":
-    print("This module is not for direct call!")
+	print("This module is not for direct call!")
