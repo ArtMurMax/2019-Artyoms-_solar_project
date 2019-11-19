9f0dff02d70dbb1b43a078097182b0fc63df2250
= <=>
2019-11-20 01:01:41 +0300

    Теперь планеты летают, и даже примерно как надо. Тем не менее, спустя время они улетают далеко и надолго, а вернее сначала все ближе к центру масс, а затем на бесконечность. Происходит это из-за довольно примитивной модели рассчета через силу взаимодействия. Особенно это заметно на двойной звезде при равных скоростях. Запись в файл пока еще не реализована.

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,8 @@ def calculate_force(body, space_objects):
 		if body == obj:
 			continue  # тело не действует гравитационной силой на само себя!
 		r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-		body.Fx += gravitational_constant * body.m * obj.m / r ** 3 * (body.x - obj.x)
-		body.Fy += gravitational_constant * body.m * obj.m / r ** 3 * (body.y - obj.y)
+		body.Fx += gravitational_constant * body.m * obj.m / r ** 3 * (obj.x - body.x)
+		body.Fy += gravitational_constant * body.m * obj.m / r ** 3 * (obj.y - body.y)
 
 
 def move_space_object(body, dt):
@@ -32,11 +32,12 @@ def move_space_object(body, dt):
 	"""
 
 	ax = body.Fx/body.m
-	body.x += body.Vx * dt + ax * dt ** 2 / 2
 	body.Vx += ax * dt
+	body.x += body.Vx * dt + ax * dt ** 2 / 2
 	ay = body.Fy/body.m
-	body.y += body.Vy * dt + ay * dt ** 2 / 2
 	body.Vy += ay * dt
+	body.y += body.Vy * dt + ay * dt ** 2 / 2
+
 
 
 def recalculate_space_objects_positions(space_objects, dt):
